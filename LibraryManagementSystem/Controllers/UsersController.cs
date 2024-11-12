98b377865d899c47e14988c777fd53733b11604b
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-11-12 15:51:00 -0300

    Add Controllers for Book and User

--- LibraryManagementSystem/Controllers/UsersController.cs
+++ LibraryManagementSystem/Controllers/UsersController.cs
@@ -1,33 +1,25 @@
 ﻿using LibraryManagementSystem.Application.Models;
-using LibraryManagementSystem.Infrastructure.Persistence;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.API.Controllers
 {
-    [Route("api/books")]
     [ApiController]
-    public class BooksController : ControllerBase
+    [Route("api/user")]
+    public class UsersController : ControllerBase
     {
-        private readonly LibraryManagementSystemDbContext _context;
-
-        public BooksController(LibraryManagementSystemDbContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
-        public IActionResult Get() {
+        public IActionResult Get()
+        {
             return Ok();
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById(int id) {
+        public IActionResult GetById(int id) { 
             return Ok();
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] CreateBookInputModel createproject)
+        public IActionResult Post([FromBody] CreateUserInputModel createUser)
         {
             return Ok();
         }
