da7c790538c74a9d537777b53166e0a82e5c17d8
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-02-26 20:17:28 -0300

    Applying CQRS in User

---
+++ LibraryManagementSystem.Application/Commands/InsertUser/InsertUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagementSystem.Application.Models;
+using LibraryManagementSystem.Core.Entities;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.InsertUser
+{
+    public class InsertUserCommand : IRequest<ResultViewModel<int>>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public User ToEntity()
+            => new(Nome, Email, BirthDate);
+
+    }
+}
---
+++ LibraryManagementSystem.Application/Commands/InsertUser/InsertUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagementSystem.Application.Models;
+using LibraryManagementSystem.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application.Commands.InsertUser
+{
+    public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
+    {
+        private readonly IUserRepository _repository;
+
+        public InsertUserHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = request.ToEntity();
+
+            await _repository.Add(user);
+
+            return ResultViewModel<int>.Success(user.Id);
+        }
+    }
+}
