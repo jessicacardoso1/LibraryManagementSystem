86bc395612f429d09e2c6f2a2eb23ba91c8df732
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-03-10 08:19:12 -0300

    Create ApplicationModule.cs

---
+++ LibraryManagementSystem.Application/ApplicationModule.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagementSystem.Application.Commands.InsertUser;
+using LibraryManagementSystem.Application.Models;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddHandlers();
+            return services;
+        }
+        private static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            config.RegisterServicesFromAssemblyContaining<InsertUserCommand>());
+
+            return services;
+        }
+    }
+}
