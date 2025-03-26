87f9f6cfd7bc0f34e0e7ecf04971eef1aea3b372
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-03-26 18:51:48 -0300

    Adding first version of index book

---
+++ LibraryManagementSystem.WEB/Components/Pages/Books/Index.razor.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagementSystem.WEB.Repositories.Interfaces;
+using LibraryManagementSystem.WEB.Services;
+using LibraryManagementSystem.WEB.Services.Interfaces;
+using LibraryManager.Site.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace LibraryManagementSystem.WEB.Components.Pages.Books
+{
+    public class IndexBookPage : ComponentBase
+    {
+        [Inject]
+        public IBookService Service { get; set; } = null!;
+        public List<BookViewModel> Books { get; set; }
+        protected string SearchTerm = string.Empty;
+        protected bool IsLoading = false;
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadBooks();
+        }
+        private async Task LoadBooks()
+        {
+            IsLoading = true;
+            var result = await Service.GetAllBooks(SearchTerm);
+            if (result.IsSucces)
+            {
+                Books = result.Data!;
+            }
+            IsLoading = false;
+
+        }
+    }
+}
