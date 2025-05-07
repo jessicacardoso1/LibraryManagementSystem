9b4ae6a8b5e818e24f052f094b5443850cac24da
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-07 13:50:06 -0300

    Creating service to to create hash and token

---
+++ LibraryManagementSystem.Infrastructure/Auth/IAuthService.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure.Auth
+{
+    public interface IAuthService
+    {
+        string ComputeHash(string passworrd);
+        string GenerateToken(string email, string role);
+
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string ComputeHash(string passworrd)
+        {
+            using (var hash = SHA256.Create())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(passworrd);
+
+                var hashBytes = hash.ComputeHash(passwordBytes);
+
+                var builder = new StringBuilder();
+
+                for (var i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public string GenerateToken(string email, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+                );
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", email),
+                new(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials,
+                claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
